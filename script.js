df72b0f616a3e0128321f0c927c2029e1167f481
Kunal Rana <96828969+KunalRana01@users.noreply.github.com>
2025-04-08 18:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var p1 = Math.floor((Math.random()*6+1));
+var p2 = Math.floor((Math.random()*6+1));
+
+document.querySelector("img").setAttribute("src" , "./images/dice"+[p1]+".png");
+
+document.querySelectorAll("img")[1].setAttribute("src" , "./images/dice"+[p2]+".png")
+
+
+if(p1>p2){
+    document.querySelector("h1").innerHTML="<h1 class='heading'> Player 1 Won !🚩</h1>"
+} else if(p1==p2){
+    document.querySelector("h1").innerHTML="<h1 class='heading'> Its a Draw !</h1>"
+} else{
+    document.querySelector("h1").innerHTML = "<h1 class='heading'> 🚩 Player 2 Won !</h1>"
+}
