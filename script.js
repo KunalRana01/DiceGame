de2fb64ea4806bc295a31f94f6ca5a3be748b665
Kunal Rana <96828969+KunalRana01@users.noreply.github.com>
2025-04-08 18:52:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.querySelectorAll("img")[1].setAttribute("src" , "./images/dice"+[p2]+".
 if(p1>p2){
     document.querySelector("h1").innerHTML="<h1 class='heading'> Player 1 Won !🚩</h1>"
 } else if(p1==p2){
-    document.querySelector("h1").innerHTML="<h1 class='heading'> Its a Draw !</h1>"
+    document.querySelector("h1").innerHTML="<h1 class='heading'> 😓 Its a Draw !</h1>"
 } else{
     document.querySelector("h1").innerHTML = "<h1 class='heading'> 🚩 Player 2 Won !</h1>"
 }
